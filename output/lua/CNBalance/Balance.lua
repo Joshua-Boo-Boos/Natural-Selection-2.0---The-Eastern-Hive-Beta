3233541e6cf87af3db46bf7791e37c020fbbe9cb
Joshua-Boo-Boos <joshuabrazier@hotmail.com>
2026-03-19 22:45:44 +0000

    Update Balance.lua

--- output/lua/CNBalance/Balance.lua
+++ output/lua/CNBalance/Balance.lua
@@ -479,10 +479,10 @@ kAcidRocketBombDuration = 3
 kVortexShadowStepResearchCost = 30
 kVortexShadowStepResearchTime = 90
 kVortexLifetime = 6 kVortexInitTime = 0.5
-kVortexRadius = 2
+kVortexRadius = 4
 kVortexEnergyCost = 50
 kVortexPlayerDamagePerSecond = 10 kVortexStructureDamagePerSecond = 80
-kVortexSuckinVelocityPerSecond = 42.5
+kVortexSuckinVelocityPerSecond = 30
 
 kVortexMaxDamageEndurance = 10 kVortexPerPlayerDamageEnduranceCostPerSecond = 1 --kVortexProjectileEnduranceCost = 0
 
